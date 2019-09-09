b927d24740aec76c5c328f1aaaff848638014afb
Austin Michael Regan <austinmregan@gmail.com>
2019-09-09 10:56:38 -0400

    added axios, routing and linking in app

--- martian-weather-app-web/src/components/App.js
+++ martian-weather-app-web/src/components/App.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import axios from "axios";
+import Home from "./Home.js";
+import Curiosity from "./Curiosity";
+import InSight from "./InSight";
 import "../styling/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const searchUrl = "";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curiosityWeather: [5],
+      inSightWeather: [5]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to="/">
+            <p>HOME</p>
+          </Link>
+          <Link to="/curiosity">
+            <h3>Curiosity</h3>
+          </Link>
+          <Link to="/insight">
+            <h3>InSight</h3>
+          </Link>
+        </nav>
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/curiosity"
+            exact
+            render={routerProps => (
+              <Curiosity
+                curiosityData={this.state.curiosityWeather}
+                {...routerProps}
+              />
+            )}
+          />
+          <Route
+            path="/insight"
+            exact
+            render={routerProps => (
+              <InSight
+                inSightData={this.state.inSightWeather}
+                {...routerProps}
+              />
+            )}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- martian-weather-app-web/src/index.js
+++ martian-weather-app-web/src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom";
 import "./styling/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
